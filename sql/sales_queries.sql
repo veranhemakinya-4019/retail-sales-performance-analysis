d6b5ba8ad4d85b559bf2f2d4ecc439d868382345
Veranhema <your@email.com>
2026-02-22 14:27:42 +0300

    Update cleaned dataset, SQL queries, and notebook analysis

--- sql/sales_queries.sql
+++ sql/sales_queries.sql
@@ -10,7 +10,7 @@ CREATE TABLE sales (
     total_amount DECIMAL(10,2)
 );
 
-Basic Data Check
+STEP 1: Basic Data Check
 View First Rows:
 SELECT *
 FROM sales
@@ -20,7 +20,7 @@ Check Total Records:
 SELECT COUNT(*) AS total_rows
 FROM sales;
 
-Revenue Analysis
+STEP 2: Revenue Analysis
 Total Revenue:
 SELECT SUM("total_amount") AS total_revenue
 FROM sales;
@@ -34,7 +34,7 @@ SELECT
     SUM("total_amount") / COUNT("transaction_id") AS avg_order_value
 FROM sales;
 
-Sales by Category:
+STEP 3: Sales by Category
 SELECT 
     "product_category",
     SUM("total_amount") AS total_revenue
@@ -42,7 +42,7 @@ FROM sales
 GROUP BY "product_category"
 ORDER BY total_revenue DESC;
 
-Sales by Gender:
+STEP 4: Sales by Gender
 SELECT 
     gender,
     SUM("total_amount") AS total_revenue
@@ -50,41 +50,10 @@ FROM sales
 GROUP BY gender
 ORDER BY total_revenue DESC;
 
-Monthly Sales Trend:
+STEP 5: Monthly Sales Trend
 SELECT 
     TO_CHAR(Date, 'YYYY-MM') AS month,
     SUM("total_amount") AS total_revenue
 FROM sales
 GROUP BY month
 ORDER BY month;
-
-Top 10 Customers:
-SELECT 
-    "customer_id",
-    SUM("total_amount") AS total_revenue
-FROM sales
-GROUP BY "customer_id"
-ORDER BY total_revenue DESC
-LIMIT 10;
-
-Best-Selling Categories by Quantity:
-SELECT 
-    "product_category",
-    SUM(Quantity) AS total_units_sold
-FROM sales
-GROUP BY "product_category"
-ORDER BY total_units_sold DESC;
-
-Age Group Segmentation:
-SELECT
-    CASE
-        WHEN age  < 25 THEN '18-24'
-        WHEN age  < 35 THEN '25-34'
-        WHEN age < 45 THEN '35-44'
-        WHEN age  < 55 THEN '45-54'
-        ELSE '55+'
-    END AS age_group,
-    SUM("total_amount") AS total_revenue
-FROM sales
-GROUP BY age_group
-ORDER BY total_revenue DESC;
